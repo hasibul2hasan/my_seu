a97a067f1e773952b7a3310b036fa0b918151ee3
hasibul2hasan <hasanshantoemail@gmail.com>
2024-08-25 06:56:43 +0600

    color change

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,6 @@ void main() {
   // Ensure that the framework is initialized
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Set the status bar
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor: const Color.fromRGBO(15, 23, 42, 1), // Status bar color
-    statusBarIconBrightness: Brightness.light, // Light icons on status bar
-  ));
-
   runApp(
     const MyApp(),
   );
@@ -39,11 +33,43 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
+  Color _appBarColor = const Color.fromRGBO(15, 23, 42, 1);
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 6, vsync: this);
+    _tabController.addListener(_handleTabSelection);
+  }
+
+  void _handleTabSelection() {
+    setState(() {
+      switch (_tabController.index) {
+        case 0:
+          _appBarColor = const Color.fromRGBO(15, 23, 42, 1); // UMS
+          break;
+        case 1:
+          _appBarColor = const Color.fromARGB(255, 255, 255, 255); // Classroom
+          break;
+        case 2:
+          _appBarColor = const Color.fromARGB(255, 255, 255, 255); // Mail
+          break;
+        case 3:
+          _appBarColor = const Color.fromRGBO(33, 150, 243, 1); // Map
+          break;
+        case 4:
+          _appBarColor = const Color.fromRGBO(156, 39, 176, 1); // Chat
+          break;
+        case 5:
+          _appBarColor = const Color.fromRGBO(255, 193, 7, 1); // Help
+          break;
+      }
+
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: _appBarColor, // Status bar color
+        statusBarIconBrightness: Brightness.light, // Light icons on status bar
+      ));
+    });
   }
 
   @override
@@ -52,37 +78,45 @@ class _HomeScreenState extends State<HomeScreen>
       length: 6,
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: const Color.fromRGBO(15, 23, 42, 1),
+          backgroundColor: _appBarColor,
           elevation: 0,
-          // title: const Text(
-          //   'WebView in TabBar',
-          //   style: TextStyle(color: Colors.white),
-          // ),
           bottom: PreferredSize(
-            preferredSize: Size.fromHeight(20.0), // Adjust as needed
+            preferredSize: const Size.fromHeight(20.0), // Adjust as needed
             child: TabBar(
               controller: _tabController,
-              indicator: BoxDecoration(),
-              tabs: const [
-                Tab(icon: Icon(Icons.web), text: 'SEU UMS'),
-                Tab(icon: Icon(Icons.home), text: 'ClassRoom'),
-                Tab(icon: Icon(Icons.settings), text: 'Mail'),
-                Tab(icon: Icon(Icons.settings), text: 'Map'),
-                Tab(icon: Icon(Icons.settings), text: 'Chat'),
-                Tab(icon: Icon(Icons.settings), text: 'Help'),
+              indicator: UnderlineTabIndicator(
+                borderSide: BorderSide(
+                  color: const Color.fromARGB(
+                      255, 126, 0, 0), // Set your desired color here
+                  width: 2.0, // Set the thickness of the underline
+                ),
+              ),
+              tabs: [
+                //Tab(icon: Icon(Icons.web), text: 'SEU UMS'),
+                Tab(
+                  icon: Image.asset("assets/TabBarLogo/ums.png"),
+                  //text: 'ClassRoom'
+                ),
+                Tab(
+                  icon: Image.asset("assets/TabBarLogo/google_classroom.png"),
+                  // text: 'ClassRoom',
+                ),
+                Tab(icon: Icon(Icons.mail), text: 'Note'),
+                Tab(icon: Icon(Icons.map), text: 'Map'),
+                Tab(icon: Icon(Icons.chat), text: 'Chat'),
+                Tab(icon: Icon(Icons.help), text: 'Help'),
               ],
             ),
           ),
         ),
         body: TabBarView(
           controller: _tabController,
-
           physics:
               const NeverScrollableScrollPhysics(), // Disable swipe gesture
           children: const [
             UmsTab(),
             ClassroomTab(),
-            MailTab(),
+            KeepTab(),
             MapTab(),
             ChatTab(),
             HelpTab(),
@@ -93,8 +127,7 @@ class _HomeScreenState extends State<HomeScreen>
   }
 }
 
-//UMS tab here###########################################################################
-
+// UMS tab here
 class UmsTab extends StatefulWidget {
   const UmsTab({Key? key}) : super(key: key);
 
@@ -139,7 +172,7 @@ class _UmsTabState extends State<UmsTab> with AutomaticKeepAliveClientMixin {
           right: 16,
           child: FloatingActionButton(
             onPressed: _reloadWebView,
-            child: Icon(Icons.refresh),
+            child: const Icon(Icons.refresh, color: Colors.white),
             backgroundColor: const Color.fromRGBO(15, 23, 42, 1),
           ),
         ),
@@ -148,7 +181,7 @@ class _UmsTabState extends State<UmsTab> with AutomaticKeepAliveClientMixin {
   }
 }
 
-// ClassRoom tab here ###########################################################################
+// Classroom tab here
 class ClassroomTab extends StatefulWidget {
   const ClassroomTab({Key? key}) : super(key: key);
 
@@ -159,7 +192,8 @@ class ClassroomTab extends StatefulWidget {
 class _ClassroomTabState extends State<ClassroomTab>
     with AutomaticKeepAliveClientMixin {
   late WebViewController _webViewController;
-  String initialUrl = 'https://classroom.google.com/';
+  String initialUrl =
+      'https://accounts.google.com/ServiceLogin?continue=https%3A%2F%2Fclassroom.google.com&passive=true';
 
   @override
   bool get wantKeepAlive => true;
@@ -194,8 +228,8 @@ class _ClassroomTabState extends State<ClassroomTab>
           right: 16,
           child: FloatingActionButton(
             onPressed: _reloadWebView,
-            child: Icon(Icons.refresh),
-            backgroundColor: const Color.fromRGBO(15, 23, 42, 1),
+            child: const Icon(Icons.refresh, color: Colors.black),
+            backgroundColor: const Color.fromARGB(255, 255, 255, 255),
           ),
         ),
       ],
@@ -203,18 +237,17 @@ class _ClassroomTabState extends State<ClassroomTab>
   }
 }
 
-// Mail tab here ###########################################################################
-
-class MailTab extends StatefulWidget {
-  const MailTab({Key? key}) : super(key: key);
+// Note tab here
+class KeepTab extends StatefulWidget {
+  const KeepTab({Key? key}) : super(key: key);
 
   @override
-  _MailTabState createState() => _MailTabState();
+  _KeepTabState createState() => _KeepTabState();
 }
 
-class _MailTabState extends State<MailTab> with AutomaticKeepAliveClientMixin {
+class _KeepTabState extends State<KeepTab> with AutomaticKeepAliveClientMixin {
   late WebViewController _webViewController;
-  String initialUrl = 'https://mail.google.com/mail/u/0/#inbox';
+  String initialUrl = 'https://keep.google.com/';
 
   @override
   bool get wantKeepAlive => true;
@@ -249,8 +282,8 @@ class _MailTabState extends State<MailTab> with AutomaticKeepAliveClientMixin {
           right: 16,
           child: FloatingActionButton(
             onPressed: _reloadWebView,
-            child: Icon(Icons.refresh),
-            backgroundColor: const Color.fromRGBO(15, 23, 42, 1),
+            child: const Icon(Icons.refresh, color: Colors.black),
+            backgroundColor: const Color.fromARGB(255, 255, 255, 255),
           ),
         ),
       ],
@@ -258,30 +291,28 @@ class _MailTabState extends State<MailTab> with AutomaticKeepAliveClientMixin {
   }
 }
 
-// Map tab here ###########################################################################
-
+// Map tab here
 class MapTab extends StatelessWidget {
   const MapTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Text(
-        'Settings Tab',
+        'Map Tab',
         style: TextStyle(fontSize: 24),
       ),
     );
   }
 }
 
-// Chat tab here ###########################################################################
-
+// Chat tab here
 class ChatTab extends StatelessWidget {
   const ChatTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Text(
         'Chat Tab',
         style: TextStyle(fontSize: 24),
@@ -290,14 +321,13 @@ class ChatTab extends StatelessWidget {
   }
 }
 
-// Help tab here ###########################################################################
-
+// Help tab here
 class HelpTab extends StatelessWidget {
   const HelpTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Text(
         'Help Tab',
         style: TextStyle(fontSize: 24),
