244c3c987174d0bf6c5c042da15c7cb376f8173e
hasibul2hasan <hasanshantoemail@gmail.com>
2024-08-29 15:27:54 +0600

    firebase

--- lib/screens/chat_tab.dart
+++ lib/screens/chat_tab.dart
@@ -11,36 +11,17 @@ class _ChatTabState extends State<ChatTab> with AutomaticKeepAliveClientMixin {
   @override
   bool get wantKeepAlive => true;
 
+
+
+
+
+
+
+
+  
+
   @override
   Widget build(BuildContext context) {
-    super.build(context);
-
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'Coming Soon',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'We are working hard to bring this feature to you. Stay tuned!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey[600],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
