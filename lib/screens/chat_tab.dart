a6cdb387669336fc11c81a41c0962dc8d7d9baf8
hasibul2hasan <hasanshantoemail@gmail.com>
2024-08-31 02:08:48 +0600

    Add Firebase initialization and Google sign-in

--- lib/screens/chat_tab.dart
+++ lib/screens/chat_tab.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:sign_in_button/sign_in_button.dart';
 
 class ChatTab extends StatefulWidget {
   const ChatTab({Key? key}) : super(key: key);
@@ -11,17 +13,44 @@ class _ChatTabState extends State<ChatTab> with AutomaticKeepAliveClientMixin {
   @override
   bool get wantKeepAlive => true;
 
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  User? _user;
 
-
-
-
-
-
-  
+  @override
+  void initState() {
+    super.initState();
+    _auth.authStateChanges().listen((event) {
+      setState(() {
+        _user = event;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Google sign in"),
+      ),
+      body: _user != null ? _userInfo() : _googleSignInButton(),
+    );
+  }
+
+  Widget _googleSignInButton() {
+    return Center(
+      child: SizedBox(
+        height: 50,
+        child: SignInButton(
+          Buttons.google,
+          text: "Sign up with google",
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+
+  Widget _userInfo() {
+    return SizedBox();
   }
 }
