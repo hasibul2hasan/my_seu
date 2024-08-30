68d1371a3aa1d34b826737a71b33d0cd70d3a708
hasibul2hasan <hasanshantoemail@gmail.com>
2024-08-31 05:12:27 +0600

    Add user info widget to chat tab

--- lib/screens/chat_tab.dart
+++ lib/screens/chat_tab.dart
@@ -51,7 +51,32 @@ class _ChatTabState extends State<ChatTab> with AutomaticKeepAliveClientMixin {
   }
 
   Widget _userInfo() {
-    return SizedBox();
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(_user!.photoURL!),
+              ),
+            ),
+          ),
+          Text(_user!.email!),
+          Text(_user!.displayName ?? ""),
+          MaterialButton(
+            color: Colors.red,
+            child: const Text("Sign out"),
+            onPressed: _auth.signOut,
+          )
+        ],
+      ),
+    );
   }
 
   void _handleGoogleSignIn() {
